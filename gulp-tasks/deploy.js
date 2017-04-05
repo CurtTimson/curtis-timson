b2b85a09817fffeac45ca2de4550f280a43918bd
Curtis Timson <curt@live.co.uk>
2017-04-05 20:32:31 +0100

    Update gulp deploy task

--- gulp-tasks/deploy.js
+++ gulp-tasks/deploy.js
@@ -2,6 +2,6 @@ var gulp = require("gulp");
 var ghPages = require('gulp-gh-pages');
 
 gulp.task('deploy', function() {
-  return gulp.src('./app/public/**/*')
+  return gulp.src('./public/**/*')
     .pipe(ghPages());
 });
