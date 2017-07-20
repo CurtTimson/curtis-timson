10c2e2343faf635b453743f46301a8750dd21cf1
Curtis Timson <curt@live.co.uk>
2017-07-20 19:12:53 +0100

    Add 'dev' task to gulp

--- gulp-tasks/dev.js
+++ gulp-tasks/dev.js
@@ -4,6 +4,8 @@ var less = require("gulp-less");
 
 var lessPath = 'static/less/*.less';
 
+gulp.task('dev', ['less-watch']);
+
 gulp.task('less-watch', function(){
   gulp.watch(lessPath, ['less']);
 });
