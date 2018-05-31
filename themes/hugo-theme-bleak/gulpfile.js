df76a28ebfbfe77701c8fe8718ca078fb8ccad74
Curtis Timson <curt@live.co.uk>
2018-05-31 12:19:56 +0100

    Add gulpfile for compiling scriptjs

---
+++ themes/hugo-theme-bleak/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+
+gulp.task('default', ['compile-script']);
+
+gulp.task('compile-script', () => (
+    gulp.src('src/js/script.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('static/js'))
+));
