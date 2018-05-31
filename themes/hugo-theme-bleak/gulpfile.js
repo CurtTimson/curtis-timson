16b76832b913b2f21a33ecbe8ed4217aa9e8b1ac
Curtis Timson <curt@live.co.uk>
2018-05-31 12:39:43 +0100

    Rebuild mainjs with gulp

--- themes/hugo-theme-bleak/gulpfile.js
+++ themes/hugo-theme-bleak/gulpfile.js
@@ -1,14 +1,21 @@
 var gulp = require("gulp");
 var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
 
-gulp.task('default', ['compile-script']);
+gulp.task('default', ['compile-scriptjs']);
 
-gulp.task('compile-script', () => (
-    gulp.src('src/js/script.js')
+gulp.task('compile-scriptjs', () => {
+    return gulp.src('src/js/script.js')
         .pipe(uglify())
         .pipe(gulp.dest('static/js'))
-));
+});
 
 gulp.task('dev', () => {
-    gulp.watch('src/js/script.js', ['compile-script']);
+    gulp.watch('src/js/script.js', ['compile-scriptjs', 'compile-scripts']);
 })
+
+gulp.task('compile-scripts', () => {
+    return gulp.src(['static/js/dependencies.js', 'static/js/script.js'])
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('static/js'))
+});
