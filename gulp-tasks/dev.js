62a9221961ebcba7250845fa6bde6edc6a78eb05
Curtis Timson <curt@live.co.uk>
2018-06-07 10:09:34 +0100

    Add new ct-main compiled css

--- gulp-tasks/dev.js
+++ gulp-tasks/dev.js
@@ -1,7 +1,8 @@
-var gulp = require("gulp");
-var less = require("gulp-less");
+let gulp = require("gulp");
+let less = require("gulp-less");
+let concat = require('gulp-concat');
 
-var lessPath = 'static/less/*.less';
+let lessPath = 'static/less/*.less';
 
 gulp.task('dev', ['less-watch']);
 
@@ -20,7 +21,15 @@ gulp.task("copy-npm-files", ['copy-npm-files-static']);
 gulp.task('copy-npm-files-static', () => {
   return gulp.src(['./node_modules/jquery/dist/jquery.min.js',
                     './node_modules/angular/angular.min.js',
-                    './node_modules/lastfm-nowplaying/dist/lastfm-nowplaying.min.js',
-                    './node_modules/lastfm-nowplaying/dist/lastfm-nowplaying.min.css'])
+                    './node_modules/lastfm-nowplaying/dist/lastfm-nowplaying.min.js'])
         .pipe(gulp.dest('./static/npm/'));
+});
+
+gulp.task('compile-ct-main-css', () => {
+  return gulp.src([
+    './node_modules/lastfm-nowplaying/dist/lastfm-nowplaying.min.css',
+    './static/css/ct.css'
+  ])
+  .pipe(concat('ct-main.css'))
+  .pipe(gulp.dest('./static/css'));
 });
