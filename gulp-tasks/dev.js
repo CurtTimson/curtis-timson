696ea48feb357456d2a5e816b494901ac29d4136
Curtis Timson <curt@live.co.uk>
2018-06-07 10:19:14 +0100

    Perf improvements (#34)
    * Remove deploy task
    
    * Remove copy theme files
    
    * Add new ct-main compiled css
    
    * Rename hugo-theme-bleak to custom folder
    
    * Update config preview toml
    
    * Add cleanCSS step to ct-main

--- gulp-tasks/dev.js
+++ gulp-tasks/dev.js
@@ -1,7 +1,9 @@
-var gulp = require("gulp");
-var less = require("gulp-less");
+let gulp = require("gulp");
+let less = require("gulp-less");
+let concat = require('gulp-concat');
+let cleanCSS = require('gulp-clean-css');
 
-var lessPath = 'static/less/*.less';
+let lessPath = 'static/less/*.less';
 
 gulp.task('dev', ['less-watch']);
 
@@ -15,17 +17,21 @@ gulp.task('less', function(){
     .pipe(gulp.dest('static/css'))
 });
 
-gulp.task("copy-npm-files", ['copy-npm-files-static', 'copy-npm-files-theme']);
+gulp.task("copy-npm-files", ['copy-npm-files-static']);
 
 gulp.task('copy-npm-files-static', () => {
   return gulp.src(['./node_modules/jquery/dist/jquery.min.js',
                     './node_modules/angular/angular.min.js',
-                    './node_modules/lastfm-nowplaying/dist/lastfm-nowplaying.min.js',
-                    './node_modules/lastfm-nowplaying/dist/lastfm-nowplaying.min.css'])
+                    './node_modules/lastfm-nowplaying/dist/lastfm-nowplaying.min.js'])
         .pipe(gulp.dest('./static/npm/'));
 });
 
-gulp.task('copy-npm-files-theme', () => {
-  return gulp.src(['./node_modules/hugo-theme-bleak/**/**.**'])
-        .pipe(gulp.dest('./themes/hugo-theme-bleak'));
+gulp.task('compile-ct-main-css', () => {
+  return gulp.src([
+    './node_modules/lastfm-nowplaying/dist/lastfm-nowplaying.min.css',
+    './static/css/ct.css'
+  ])
+  .pipe(cleanCSS())
+  .pipe(concat('ct-main.css'))
+  .pipe(gulp.dest('./static/css'));
 });
